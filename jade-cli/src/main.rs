41a539f5fa3e17974bc136f279162353ffc03419
Marek Freunscht <freunscht58@outlook.de>
2024-12-22 03:36:38 +0100

    jade-cli: Update example to use cycle stepping

--- jade-cli/src/main.rs
+++ jade-cli/src/main.rs
@@ -5,17 +5,18 @@ fn main() {
     let mut cpu: Cpu = Cpu::new();
 
     cpu.current_instr = 0x20;
-    cpu.pc = 3;
-    cpu.bus.data[2] = 0x20; // JSR abs, but isn't actually used for now
+    cpu.pc = 2;
+    cpu.next_pc = 2; // TODO: should not be needed to set next_pc manually, maybe have some init method
+    cpu.bus.data[2] = 0x20;
     cpu.bus.data[3] = 0x01;
     cpu.bus.data[4] = 0x10;
 
-    for _ in 0..6 {
+    for _ in 0..7 {
         let cycle = INSTRUCTIONS[cpu.current_instr].cycles[cpu.current_instr_step];
-        cpu.execute_microcode_step();
+        cpu.step_cycle();
         println!(
-            "ab: {:04x}, db: {:02x}, r: {}, pc: {:04x}, sp: {:02x}, {cycle}",
-            cpu.ab, cpu.db, cpu.r, cpu.pc, cpu.sp
+            "ab: {:04x}, db: {:02x}, r: {}, pc: {:04x}, sp: {:02x}, {cycle}, {}",
+            cpu.ab, cpu.db, cpu.r, cpu.pc, cpu.sp, cpu.execution_state
         );
     }
 }
