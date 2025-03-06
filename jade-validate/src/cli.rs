917977235772df3833671b0187006f3980057bc9
Marek Freunscht <freunscht58@outlook.de>
2025-03-06 03:16:35 +0100

    jade-validate: Add docstrings for clap args and commands

--- jade-validate/src/cli.rs
+++ jade-validate/src/cli.rs
@@ -15,10 +15,13 @@ pub struct Cli {
 #[derive(Subcommand)]
 pub enum Command {
     Validate {
+        /// The validating emulator. Possible options are <Perfect6502>.
         #[arg(long, short)]
         validator: ValidatorType,
+        /// The emulator to be validated. Possible options are <Jade>.
         #[arg(long, short)]
         generator: GeneratorType,
+        /// The amount of cycles that the validation runs for.
         #[arg(long, short)]
         cycles: usize,
         #[command(subcommand)]
@@ -29,12 +32,19 @@ pub enum Command {
 #[derive(Subcommand)]
 #[command(rename_all = "kebab-case")]
 pub enum ExecutableCommand {
+    /// Loads a builtin program
     #[command(alias = "p")]
-    WithBuiltin { name: String },
+    WithBuiltin {
+        /// The builtin program to execute. Possible values are <md5>.
+        name: String,
+    },
+    /// Loads a program from a file
     #[command(alias = "f")]
     WithFile {
+        /// The name of the file which contains the executable.
         #[arg(long, short)]
         name: PathBuf,
+        /// The address at which to load the program. Can be hex or decimal.
         #[arg(long, short, value_parser=maybe_hex::<u16>)]
         start_addr: u16,
     },
